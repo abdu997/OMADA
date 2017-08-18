90d2b6a97da8d277c2db6adbf727e60be3d2014e
ybce <yousef51@hotmail.co.uk>
2017-08-18 18:37:14 -0400

    Various changes

--- chat/table.sql
+++ chat/table.sql
@@ -1,8 +1,26 @@
- CREATE TABLE IF NOT EXISTS `linkRepository` (  
-  `id` int(11) NOT NULL AUTO_INCREMENT,  
-  `team_id` int(11) NOT NULL,  
+CREATE TABLE `chatroom_user` (
+  `chatconn_id` int(11) NOT NULL AUTO_INCREMENT,
+  `chatroom_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `link` varchar(200) NOT NULL,
-  `note` varchar(200) NOT NULL, 
-  PRIMARY KEY (`id`)  
- ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
+  `team_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`chatconn_id`,`user_id`,`chatroom_id`),
+  KEY `fk_1_idx` (`chatroom_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1
+
+
+CREATE TABLE `chatrooms` (
+  `chatroom_id` int(10) NOT NULL AUTO_INCREMENT,
+  `team_id` decimal(11,0) DEFAULT NULL,
+  `chatroom_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`chatroom_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1
+
+CREATE TABLE `messages` (
+  `messages_id` int(11) NOT NULL AUTO_INCREMENT,
+  `chatroom_id` int(11) DEFAULT NULL,
+  `class` varchar(45) DEFAULT NULL,
+  `sender` int(11) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `message` text,
+  PRIMARY KEY (`messages_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1
