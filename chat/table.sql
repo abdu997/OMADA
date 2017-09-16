7f910da85d27d77c1fea5e8dc91911b5153637c1
abdu997 <amoud@Abduls-MacBook-Air.local>
2017-09-16 06:42:10 -0400

    Project manager MySQL scripts pushed

--- chat/table.sql
+++ chat/table.sql
@@ -1,27 +1,19 @@
-CREATE TABLE IF NOT EXISTS `chatroom_user` (
-  `chatconn_id` int(11) NOT NULL AUTO_INCREMENT,
-  `chatroom_id` int(11) NOT NULL,
+CREATE TABLE `progress_record` (
+  `record_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `team_id` int(11) NOT NULL,
-  PRIMARY KEY (`chatconn_id`,`user_id`,`chatroom_id`),
-  KEY `fk_1_idx` (`chatroom_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1
+  `goal_id` int(11) NOT NULL,
+  `record` varchar(500) NOT NULL,
+  `initial_record` varchar(1) NOT NULL,
+  `timestamp` varchar(45) NOT NULL,
+  PRIMARY KEY (`record_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1
 
-
-CREATE TABLE IF NOT EXISTS `chatrooms` (
-  `chatroom_id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `team_goal` (
+  `goal_id` int(11) NOT NULL AUTO_INCREMENT,
   `team_id` int(11) NOT NULL,
-  `chatroom_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`chatroom_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1
-
-CREATE TABLE IF NOT EXISTS `messages` (
-  `messages_id` int(11) NOT NULL AUTO_INCREMENT,
-  `chatroom_id` int(11) NOT NULL,
-  `class` varchar(45) NOT NULL,
-  `sender` int(11) NOT NULL,
-  `timestamp` varchar(40) NOT NULL,
-  `message` text(500) NOT NULL,
-  `initial_message` varchar(45) NOT NULL,
-  PRIMARY KEY (`messages_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1
+  `status` varchar(200) NOT NULL,
+  `goal` varchar(200) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`goal_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=37 DEFAULT CHARSET=latin1
