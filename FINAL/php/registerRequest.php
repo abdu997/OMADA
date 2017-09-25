af2bab270256fe3b7732b72f9a4d0b8cf46b2d68
ybce <yousef51@hotmail.co.uk>
2017-09-25 19:47:24 -0400

    Added check to see if email already exists in DB

--- FINAL/php/registerRequest.php
+++ FINAL/php/registerRequest.php
@@ -4,6 +4,10 @@ $data = json_decode(file_get_contents("php://input"));
 
 if (count($data) > 0){
     $email = mysqli_real_escape_string($connect, $data->email);
+	$sql = "SELECT email from test.users WHERE email = '$email'";
+	$result = mysqli_query($connect,$sql);
+	$count = mysqli_num_rows($result);
+	if($count == 0){
 //    $password = mysqli_real_escape_string($connect, $_POST['password']);
     $first_name = mysqli_real_escape_string($connect, $data->first_name);
     $last_name = mysqli_real_escape_string($connect, $data->last_name);
@@ -19,5 +23,9 @@ if (count($data) > 0){
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($connect);
     }
+	}
+	else{
+		echo "Email already exists!";
+	}
 }
 ?>
