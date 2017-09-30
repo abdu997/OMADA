d0b8cf99381d0d675a4141d32382bfeb2074bc08
abdu997 <amoud@Abduls-MacBook-Air.local>
2017-09-30 17:46:53 -0400

    Fixed team name issue
    Team and personal dashboards are not displayed dynamically on the UI,
    and finished the UI for edit profile.

--- FINAL/nav-sidebar.php
+++ FINAL/nav-sidebar.php
@@ -1,4 +1,4 @@
-    <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar" ng-controller="SessionController" ng-init="userTeams(); userinfo()">
+    <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:250px;" id="mySidebar" ng-controller="SessionController" ng-init="userTeams(); userinfo()">
         <br>
         <div class="w3-container w3-row" ng-repeat="x in user" >
             <div class="w3-col s4">
@@ -12,9 +12,9 @@
         <div class="w3-bar-block">
             <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>&nbsp; Close Menu</a>
             <div ng-repeat="x in teams | filter : {'type':'personal'} " style="text-transform: capitalize; color: white;">
-                <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw"></i>&nbsp; {{x.team_id}}</a>
+                <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw"></i>&nbsp; {{x.team_name}}</a>
             </div>
-            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog fa-fw"></i>&nbsp; Account Settings</a>
+            <a onclick="document.getElementById('edit_user').style.display='block'" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog fa-fw"></i>&nbsp; Account Settings</a>
             <a onclick="document.getElementById('team_create').style.display='block'" class="w3-bar-item w3-button w3-padding"><i class="fa fa-plus fa-fw"></i>&nbsp; Create Team</a>
             <a href="php/logout.php" class="w3-bar-item w3-button w3-padding logout"><i class="fa fa-sign-out fa-fw"></i>&nbsp; Log Out</a>
             <hr>
@@ -46,6 +46,23 @@
                         <input class="w3-button" type="submit" value="Create" style="background: white; margin-top: 10px">
                         <p><small>NOTE: You will be the Admin for the team. You can set up a team without members, and add members later.</small>
                         </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div id="edit_user" class="w3-modal">
+            <div  ng-controller="SessionController" ng-init="userinfo()"  class="w3-modal-content w3-animate-top w3-card-4" style="padding: 25px; background: #f1f1f1!important; width: 30%;">
+                <span onclick="document.getElementById('edit_user').style.display='none'" class="w3-button w3-display-topright" style="font-size: 20px">&times;</span>
+                <div class="w3-container">
+                    <h4>Edit Personal Profile</h4>
+                    <form ng-repeat="x in user">
+                        <label>First Name</label>
+                        <input value="{{x.first_name}}" class="w3-input w3-border-0" type="text" required>
+                        <label>Last Name</label>
+                        <input value="{{x.last_name}}" class="w3-input w3-border-0" type="text" required>
+                        <label>Email</label>
+                        <input value="{{x.email}}" class="w3-input w3-border-0" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
+                        <input class="w3-button" type="submit" value="Update" style="background: white; margin-top: 10px">
                     </form>
                 </div>
             </div>
--- FINAL/php/userTeam.php
+++ FINAL/php/userTeam.php
@@ -3,16 +3,17 @@ include("connect.php");
 session_start();
 $user_id = $_SESSION['user_id'];
 $output = array();
-$query  = "SELECT * FROM team_user WHERE u_id='$user_id' ";
+$query  = "SELECT * FROM team_user WHERE u_id='$user_id'";
 $result = mysqli_query($connect, $query);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_array($result)) {
-        $t_id = $row[0];
-        $sql = "SELECT team_id, team_name, type FROM team WHERE team_id = '$t_id'";
-        $rows = mysqli_fetch_array(mysqli_query($connect,$sql));
-        $team_name = $rows[0];     
-        $out = array('0' => '0', 'team_id' => $rows[0], 'team_name' => $rows[1]);
-        $output[] = $out;  
+        $team_id =  $row[0];
+        $sql = "SELECT * FROM team WHERE team_id='$team_id'";
+        $res = mysqli_query($connect, $sql);
+        $team = mysqli_fetch_array($res);
+        $row["team_name"] = $team[1];
+        $row["type"] = $team[2];
+        $output[] = $row;
     }
     echo json_encode($output);
 }
