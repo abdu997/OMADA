7e43f9dcfd9d461c80c3905cbbedc1cd59e20295
abdu997 <amoud@Abduls-MacBook-Air.local>
2017-10-18 01:34:01 -0400

    Minor fixes
    Deleted username column in users table. Changed column number
    accordingly.

--- FINAL/php/session.php
+++ FINAL/php/session.php
@@ -2,5 +2,4 @@
 session_start();
 $user_id = $_SESSION['user_id'];
 $team_id = $_SESSION['team_id'];
-echo $team_id;
 ?>
--- FINAL/php/userTeam.php
+++ FINAL/php/userTeam.php
@@ -7,12 +7,13 @@ $query  = "SELECT * FROM team_user WHERE u_id='$user_id'";
 $result = mysqli_query($connect, $query);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_array($result)) {
-        $team_id =  $row[0];
+        $team_id =  $row[1];
         $sql = "SELECT * FROM team WHERE team_id='$team_id'";
         $res = mysqli_query($connect, $sql);
         $team = mysqli_fetch_array($res);
         $row["team_name"] = $team[1];
         $row["type"] = $team[2];
+        $row["plan"] = $team[3];
         $output[] = $row;
     }
     echo json_encode($output);
