aaadab2e702709f8b12fc881b3ac6d552a550517
abdu997 <aamou094@uottawa.ca>
2017-10-23 04:15:18 -0400

    Check if the person switching the admin is an actual admin before making changes to team configuration.

--- FINAL/php/removeMember.php
+++ FINAL/php/removeMember.php
@@ -3,24 +3,29 @@ include'connect.php';
 $data = json_decode(file_get_contents("php://input"));
 session_start();
 $team_id = $_SESSION['team_id'];
-if(count($data)>0){
-    if($data -> team_connecuser_id == null){
-        $email = mysqli_real_escape_string($connect, $data->email);
-        $sql = "DELETE FROM team_nonuser WHERE email = '$email' AND team_id = '$team_id'";
-        if(mysqli_query($connect, $sql)){
-            echo"success1";
+$admin_status = $_SESSION['admin_status'];
+if($admin_status == 'Y'){
+    if(count($data)>0){
+        if($data -> team_connect_id == null){
+            $email = mysqli_real_escape_string($connect, $data->email);
+            $sql = "DELETE FROM team_nonuser WHERE email = '$email' AND team_id = '$team_id'";
+            if(mysqli_query($connect, $sql)){
+                echo"success1";
+            } else {
+                echo"error1";
+            }
         } else {
-            echo"error1";
-        }
-    } else {
-        $team_connecuser_id = $data -> team_connecuser_id;
-        $sql2 = "DELETE FROM team_user WHERE team_connect_id= '$team_connect_id'";
-        if (mysqli_query($connect, $sql2)){
-            echo "success";
-        } else {
-            echo "error";
+            $team_connect_id = $data -> team_connect_id;
+            $sql2 = "DELETE FROM team_user WHERE team_connect_id= '$team_connect_id'";
+            if (mysqli_query($connect, $sql2)){
+                echo "success";
+            } else {
+                echo "error";
+            }
         }
     }
+} else {
+    echo"You are not an admin";
 }
 
 ?>
