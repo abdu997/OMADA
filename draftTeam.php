2b8aab436d0ce6c5f87fa1c948d9c0205ac8495f
abdu997 <aamou094@uottawa.ca>
2017-10-26 20:48:30 -0400

    Deleted unnecessary files

--- draftTeam.php
+++
@@ -1,7 +0,0 @@
-<div ng-app="myapp" ng-controller="ChatController" ng-init="getMembers()">
-<!--    <div ng-include="'chat.php'"></div>-->
-    <?php include'chat/index.php'; ?>
-</div>
-<script src="js/angular.min.js"></script>
-<script src="js/jquery.js"></script>
-<script src="chat.js"></script>
