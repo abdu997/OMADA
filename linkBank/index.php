2a22476c42b6783a0e2d1067e1ebe377c5385f3e
abdu997 <aamou094@uottawa.ca>
2017-11-30 03:17:53 -0500

    Code cleanup

--- linkBank/index.php
+++ linkBank/index.php
@@ -13,35 +13,35 @@
         }
     </style>
 </head>
-    <div ng-app="myapp" ng-controller="linkRepository" ng-init="displayData()">
-        <form name="link_form2">
-            <label>Link</label>
-            <input type="text" ng-model="link">
-            <br>
-            <label>Note</label>
-            <input type="text" ng-model="note">
-            <br>
-            <input type="submit" ng-disabled="link_form2.$invalid" ng-click="insertLink()" value="{{btnName}}" />
-        </form>
-        <br />
-        <br />
-        <table>
-            <tr>
-                <th>Member</th>
-                <th>Link</th>
-                <th>Notes</th>
-                <th></th>
-            </tr>
-            <tr ng-repeat="x in links | filter: '.'">
-                <td>{{x.user}}</td>
-                <td ng-bind-html="x.url"></td>
-                <td>{{x.note}}</td>
-                <td>
-                    <i ng-click="updateData(x.record_id, x.link, x.note)" class="fa fa-pencil-square-o fw"></i>
-                    <i ng-click="deleteData(x.record_id)" class="fa fa-trash fw trash"></i>
-                </td>
-            </tr>
-        </table>
-    </div>
+<div ng-app="myapp" ng-controller="linkRepository" ng-init="displayData()">
+    <form name="link_form2">
+        <label>Link</label>
+        <input type="text" ng-model="link">
+        <br>
+        <label>Note</label>
+        <input type="text" ng-model="note">
+        <br>
+        <input type="submit" ng-disabled="link_form2.$invalid" ng-click="insertLink()" value="{{btnName}}" />
+    </form>
+    <br />
+    <br />
+    <table>
+        <tr>
+            <th>Member</th>
+            <th>Link</th>
+            <th>Notes</th>
+            <th></th>
+        </tr>
+        <tr ng-repeat="x in links | filter: '.'">
+            <td>{{x.user}}</td>
+            <td ng-bind-html="x.url"></td>
+            <td>{{x.note}}</td>
+            <td>
+                <i ng-click="updateData(x.record_id, x.link, x.note)" class="fa fa-pencil-square-o fw"></i>
+                <i ng-click="deleteData(x.record_id)" class="fa fa-trash fw trash"></i>
+            </td>
+        </tr>
+    </table>
+</div>
 
 
